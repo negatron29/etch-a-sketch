0437b02f0207dd047065f25f2deae58df582a828
negatron29 <giancarlo.cusano@icloud.com>
2020-04-23 22:27:10 -0400

    checkerboard achieved

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//Populate the divs into the master container
+//using the number ultimately given by user
 function makeDivs(nbr) {
     let s = 1;
     let d = 2;
+    let idCounter =  1;
     for (let n = 0; n < nbr; n++) {
     
         let x = 1;
@@ -14,32 +17,87 @@ function makeDivs(nbr) {
             newDiv.style.gridColumnEnd = d;
             newDiv.style.gridRowStart = x;
             newDiv.style.gridRowEnd = y;
-            if (d % 2 === 0 && x %2 >0) {
+            /*if (y % 2 === 0 && d % 2 === 0) {
                 newDiv.classList.add('slave');
             }
             else {
                 newDiv.classList.add('alt');
-            }
+            }*/
             //newDiv.innerHTML = 'This is a new div!';
+            newDiv.setAttribute("id",idCounter);
             mstr.appendChild(newDiv);
             x += 1;
             y += 1;
+            idCounter += 1;
             //console.log("The value of x is " + x);
             //console.log("The value of y is " + y);
         }
     s +=1;
     d +=1;
     }
-    console.log('Complete');
+    //console.log('Complete');
+}
+
+//Build array of div IDs where the coloring pattern
+//needs to be reversed
+function popArray(nbr) {
+    let breakCol = [1];
+    let iterLim = nbr - 1;
+    for (let i = 1; i <= iterLim; i++) {
+        nextBrk = (nbr * i) + 1;
+        breakCol.push(nextBrk);
+    }
+    console.log(breakCol);
+    return breakCol;
+}
+
+//Function to apply primary pattern via
+//class assignment
+function colorPrimary(startCol,len) {
+    let endPt = startCol + len - 1;
+    for (let i = startCol; i <= endPt; i++) {
+        let modNow = document.getElementById(i);
+        //console.log(modNow);
+        if (i % 2 ===0) {
+            modNow.classList.add("slave");
+        }
+        else {
+            modNow.classList.add("alt");
+        }
+    }
+}
+
+//Function to apply alternate pattern via
+//class assignment
+function colorAlt(startCol,len) {
+    let endPt = startCol + len - 1;
+    for (let i = startCol; i <= endPt; i++) {
+        let modNow = document.getElementById(i);
+        //console.log(modNow);
+        if (i % 2 ===0) {
+            modNow.classList.add("alt");
+        }
+        else {
+            modNow.classList.add("slave");
+        }
+    }
+}
+
+//This function controls which pattern is applied
+//to which column of divs via the pattern-applying
+//functions above
+function colorDivs (arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let startCol = arr[i];
+        if (i % 2 === 0) {
+            colorPrimary(startCol,len);
+        }
+        else {
+            colorAlt(startCol,len);
+        }
+    }
 }
 
 makeDivs(16);
-
-/*
-let a = document.querySelector("#mainDiv");
-let p = document.querySelector(".p1");
-let mstrClass = document.getElementsByTagName("div");
-console.log(a);
-console.log(p);
-console.log(mstrClass);
-*/
+colorDivs(popArray(16));
